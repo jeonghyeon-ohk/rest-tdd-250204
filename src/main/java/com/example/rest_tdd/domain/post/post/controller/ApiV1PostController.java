54909d92967259b88f582a5b39a8e09b5fcef17d
jeonghyeon-ohk <ok6737@naver.com>
2025-02-05 15:43:03 +0900

    published g

--- src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
@@ -1,6 +1,7 @@
 package com.example.rest_tdd.domain.post.post.controller;
 
 import com.example.rest_tdd.domain.member.member.entity.Member;
+import com.example.rest_tdd.domain.member.member.repository.MemberRepository;
 import com.example.rest_tdd.domain.post.post.dto.PostDto;
 import com.example.rest_tdd.domain.post.post.entity.Post;
 import com.example.rest_tdd.domain.post.post.service.PostService;
@@ -23,9 +24,13 @@ public class ApiV1PostController {
     @GetMapping("{id}")
     public RsData<PostDto> getItem(@PathVariable long id) {
 
+        Member actor = rq.getAuthenticatedActor();
+
         Post post = postService.getItem(id).orElseThrow(
                 () -> new ServiceException("404-1", "존재하지 않는 글입니다.")
         );
+
+        post.canRead(actor);
 
         return new RsData<>(
                 "200-1",
@@ -34,14 +39,17 @@ public class ApiV1PostController {
         );
     }
 
-    record WriteReqBody(@NotBlank String title, @NotBlank String content) {
+    record WriteReqBody(@NotBlank String title,
+                        @NotBlank String content,
+                        boolean published
+                        ) {
     }
 
     @PostMapping
     public RsData<PostDto> write(@RequestBody @Valid WriteReqBody reqBody) {
 
         Member actor = rq.getAuthenticatedActor();
-        Post post = postService.write(actor, reqBody.title(), reqBody.content());
+        Post post = postService.write(actor, reqBody.title(), reqBody.content(), reqBody.published());
 
         return new RsData<>(
                 "201-1",
@@ -88,4 +96,6 @@ public class ApiV1PostController {
                 "%d번 글 삭제가 완료되었습니다.".formatted(id)
         );
     }
+
+    private final MemberRepository memberRepository;
 }
