bf7885145cc0b2c6d95b5e5a9a7de338aeaa1de7
jeonghyeon-ohk <ok6737@naver.com>
2025-02-05 15:07:18 +0900

    delete no apikey

--- src/test/java/com/example/rest_tdd/ApiV1PostControllerTest.java
+++ src/test/java/com/example/rest_tdd/ApiV1PostControllerTest.java
@@ -296,7 +296,7 @@ public class ApiV1PostControllerTest {
     }
 
     @Test
-    @DisplayName("글 삭제")
+    @DisplayName("글 삭제1")
     void delete1() throws Exception {
 
         long postId = 1;
@@ -311,5 +311,22 @@ public class ApiV1PostControllerTest {
                 .andExpect(jsonPath("$.code").value("200-1"))
                 .andExpect(jsonPath("$.msg").value("%d번 글 삭제가 완료되었습니다.".formatted(postId)));
 
+    }
+
+    @Test
+    @DisplayName("글 삭제2 - no apikey")
+    void delete2() throws Exception {
+
+        long postId = 1;
+        String apiKey = "asdfasdf";
+
+        ResultActions resultActions = deleteRequest(postId, apiKey);
+
+        resultActions
+                .andExpect(status().isUnauthorized())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("delete"))
+                .andExpect(jsonPath("$.code").value("401-1"))
+                .andExpect(jsonPath("$.msg").value("잘못된 인증키입니다.".formatted(postId)));
     }
 }
